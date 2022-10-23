b8038f2444c94e41f51107ceea7bcc229433b346
abhizeit <abhijeitsarraf@gmail.com>
2022-10-23 13:36:29 +0530

    added github stats

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-//javascript for navigation bar effects on scroll
-window.addEventListener("scroll", function(){
+//responsive navbar
+
+window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
-  header.classList.toggle('sticky', window.scrollY > 0);
+  header.classList.toggle("sticky", window.scrollY > 0);
 });
 
 //javascript for responsive navigation sidebar menu
 const menuBtn = document.querySelector(".menu-btn");
 const navigation = document.querySelector(".navigation");
-const navigationItems = document.querySelectorAll(".navigation a")
+const navigationItems = document.querySelectorAll(".navigation a");
 
-menuBtn.addEventListener("click",  () => {
+menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("active");
   navigation.classList.toggle("active");
 });
@@ -24,7 +25,7 @@ navigationItems.forEach((navigationItem) => {
 //javascript for scroll to top button
 const scrollBtn = document.querySelector(".scrollToTop-btn");
 
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
   scrollBtn.classList.toggle("active", window.scrollY > 500);
 });
 
@@ -37,15 +38,15 @@ scrollBtn.addEventListener("click", () => {
 //javascript for reveal website elements on scroll
 window.addEventListener("scroll", reveal);
 
-function reveal(){
+function reveal() {
   var reveals = document.querySelectorAll(".reveal");
 
-  for(var i = 0; i < reveals.length; i++){
+  for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var revealTop = reveals[i].getBoundingClientRect().top;
     var revealPoint = 50;
 
-    if(revealTop < windowHeight - revealPoint){
+    if (revealTop < windowHeight - revealPoint) {
       reveals[i].classList.add("active");
     }
   }
